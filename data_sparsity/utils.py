ac8b908d9d2e6ae12718a175b1950aeea1fbf190
Enrico <enrico.milanese@whoi.edu>
2025-10-23 13:35:55 -0400

    Revert "Update examples.py for new API, add Jupyter notebook, improve documentation"
    This reverts commit de173a0f5c5e01c8ab968b34012c6feb76149e36.

--- data_sparsity/utils.py
+++ data_sparsity/utils.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
-"""Utility functions for data_sparsity package.
-
-This module provides helper functions for file and directory management
-used by the GenerateData class.
-"""
 
 import os
+from typing import Any, Dict, List, Tuple
 
 
 def check_or_create_folder(folder_path: str, overwrite: bool = False) -> None:
