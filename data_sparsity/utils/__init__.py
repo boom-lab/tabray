7869961d480d43df9e27031aeda066ac70d97d43
Enrico <enrico.milanese@whoi.edu>
2026-07-17 20:28:21 -0400

    ghc - refactor notebook helpers for broader use

--- data_sparsity/utils/__init__.py
+++ data_sparsity/utils/__init__.py
@@ -1,8 +1,21 @@
-"""Generator modules for data creation."""
+"""Utility modules for data creation and tutorial support."""
 
 from data_sparsity.utils.chunk_utils import ChunkUtils
+from data_sparsity.utils.viz import (
+    draw_storage_schema,
+    draw_table,
+    format_bytes,
+    format_table_value,
+    parquet_disk_size,
+    plot_grid_case,
+)
 
 __all__ = [
-    'ChunkUtils',
+    "ChunkUtils",
+    "draw_storage_schema",
+    "draw_table",
+    "format_bytes",
+    "format_table_value",
+    "parquet_disk_size",
+    "plot_grid_case",
 ]
-
